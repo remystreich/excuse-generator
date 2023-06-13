eb77cc8d40ce6e322b338436e82e4f0d98eb10b6
remystreich <remystreich@gmail.com>
2023-06-13 16:14:10 +0200

    bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,14 @@ async function submitChoice() {
     else {
     let excuses = await fetch(`https://excuser-three.vercel.app/v1/excuse/${choice}`)
     excuses = await excuses.json()
-    console.log(excuses[0].excuse);
+    
     const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
     const options = {
         method: 'POST',
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
             'Accept-Encoding': 'application/gzip',
-            'X-RapidAPI-Key': '*************************',
+            'X-RapidAPI-Key': '3f39adb57emsh5054d6959be2cc8p19e6dejsnd6ba520cf234',
             'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
         },
         body: new URLSearchParams({
@@ -34,6 +34,7 @@ async function submitChoice() {
         logArea.innerHTML = result.data.translations[0].translatedText
     } catch (error) {
         console.error(error);
+        logArea.innerHTML = excuses[0].excuse
     }
 }
 }
